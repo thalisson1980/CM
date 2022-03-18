c684ca47045e5352403c7d6e60eae340be57f826
thalisson1980 <thalisson1980@gmail.com>
2022-03-18 11:43:50 +0000

    guardar

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.cm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeName(view:View){
+        val editText = findViewById<EditText>(R.id.ed1)
+        val textView = findViewById<TextView>(R.id.tv1)
+
+        if(editText.text.toString() == ""){
+            Toast.makeText(this,"Please, write your name",Toast.LENGTH_SHORT).show()
+        }else{
+            textView.text = editText.text
+        }
+
+
+
     }
 }
 
