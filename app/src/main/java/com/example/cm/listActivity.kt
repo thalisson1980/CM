53317f5e32753761aa6daf4543317d85ef59ba15
thalisson1980 <thalisson1980@gmail.com>
2022-04-01 14:46:44 +0100

    teste

--- app/src/main/java/com/example/cm/listActivity.kt
+++ app/src/main/java/com/example/cm/listActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cm.adapters.todoAdapter
+import com.example.cm.models.todo
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list.*
+import java.time.LocalDate
 
 class listActivity : AppCompatActivity() {
 
@@ -19,6 +21,10 @@ class listActivity : AppCompatActivity() {
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
 
+        val nome = intent.getStringExtra("nome")
+        val idade = intent.getStringExtra("idade")
+        val genero = intent.getStringExtra("genero")
+        val todo = todo(nome.toString(),Integer.parseInt(idade),genero.toString())
         todoAdapter.addTodo(todo)
 
     }
