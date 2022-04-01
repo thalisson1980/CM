5b3a1278bd0e94280ec0dd566020f297cb2514fe
thalisson1980 <thalisson1980@gmail.com>
2022-04-01 14:31:58 +0100

    teste

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -1,39 +1,40 @@
 package com.example.cm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.cm.adapters.todoAdapter
+import android.view.View
+import android.widget.EditText
+
 import com.example.cm.models.todo
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.list.*
+
 import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var todoAdapter: todoAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        todoAdapter = todoAdapter(ArrayList())
-        rvTodoItems.adapter = todoAdapter
-        rvTodoItems.layoutManager = LinearLayoutManager(this)
-
 
         btnAddTodo.setOnClickListener {
             val todoNome = etNome.text.toString()
             val todoIdade = Integer.valueOf(etIdade.text.toString())
             val todoGenero =  "masculino"
             if(todoNome.isNotEmpty()){
-                val todo = todo(todoNome, todoIdade,todoGenero)
+                val intent = Intent(this, listActivity::class.java).apply {
+                    putExtra("nome",todoNome)
+                    putExtra("idade",todoIdade)
+                    putExtra("genero",todoGenero)}
+                startActivity(intent)
 
-                todoAdapter.addTodo(todo)
-                etNome.text.clear()
-                //setContentView(R.layout.list)
             }
         }
     }
+
+
+
 }
 
--- app/src/main/java/com/example/cm/listActivity.kt
+++ app/src/main/java/com/example/cm/listActivity.kt
@@ -2,10 +2,24 @@ package com.example.cm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cm.adapters.todoAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list.*
 
 class listActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: todoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        todoAdapter = todoAdapter(ArrayList())
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        todoAdapter.addTodo(todo)
+
     }
 }
