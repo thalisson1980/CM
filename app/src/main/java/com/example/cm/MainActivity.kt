42f21307b8dce62063822c20c06745452a7076ca
thalisson1980 <thalisson1980@gmail.com>
2022-04-21 10:43:30 +0100

    s

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.cm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -21,6 +24,35 @@ class MainActivity : AppCompatActivity() {
         val editText2 = findViewById<EditText>(R.id.ed2)
 
         textView.text = (Integer.parseInt(editText.text.toString()) * Integer.parseInt(editText2.text.toString()) * 1.23).toString()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+
+        inflater.inflate(R.menu.menuex3,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+
+        return when (item.itemId) {
+            R.id.item1 ->{
+                val editText = findViewById<EditText>(R.id.ed1)
+                val textView = findViewById<TextView>(R.id.tv1)
+                val editText2 = findViewById<EditText>(R.id.ed2)
+                editText.text.clear()
+                textView.text = ""
+                editText2.text.clear()
+                true
+            }
+
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
 
     }
 }
