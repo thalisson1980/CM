a6cfa9c3e800d2f91bced91a58d01b41101297de
thalisson1980 <76484672+thalisson1980@users.noreply.github.com>
2022-06-08 23:12:34 +0100

    s

--- app/src/main/java/com/example/cm/model/Person.kt
+++ app/src/main/java/com/example/cm/model/Person.kt
@@ -1,7 +1,17 @@
 package com.example.cm.model
 
-import androidx.room.*
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "person_table")
-data class Person(val name: String,) {
-}
+class Person (
+    @PrimaryKey
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "idade")
+    val idade: Int,
+    @ColumnInfo(name = "email")
+    val email: String
+)
