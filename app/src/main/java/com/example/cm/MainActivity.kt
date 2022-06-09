c7ea403ee67a7dce8a205d1d42be0b1fe7d619a8
thalisson1980 <76484672+thalisson1980@users.noreply.github.com>
2022-06-09 10:44:11 +0100

    ss

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
