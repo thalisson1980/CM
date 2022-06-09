3a14288fa2f3f706b1442024d43c10a1a08a1c24
thalisson1980 <76484672+thalisson1980@users.noreply.github.com>
2022-06-09 09:27:48 +0100

    s

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -18,58 +18,31 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreferences: SharedPreferences =  getSharedPreferences("File_1", Context.MODE_PRIVATE)
 
-        val usernameValue = sharedPreferences.getString("PREF_USERNAME","")
-        Log.d("SHARED_PREF_AULA","Found shared pref: $usernameValue")
+        val userInput = sharedPreferences.getString("userInput","")
 
-        if(usernameValue!!.isNotEmpty()){
-            showWelcome(usernameValue)
+
+        if(userInput!!.isNotEmpty()){
+            findViewById<TextView>(R.id.et1).hint =  userInput;
+        }else{
+            findViewById<TextView>(R.id.et1).hint =  "inexistente";
         }
     }
 
-    private fun showWelcome(usernameValue: String) {
-        findViewById<TextView>(R.id.tv1).text = "Hello, $usernameValue"
-        findViewById<EditText>(R.id.et1).isEnabled = false
 
-        val btn = findViewById<Button>(R.id.btn1)
-        btn.text = "Logout"
-        btn.setOnClickListener {
-            logout(it)
-        }
-    }
 
-    private fun logout(view: View) {
-        val sharedPreference: SharedPreferences = getSharedPreferences("FILE_1",Context.MODE_PRIVATE)
-        sharedPreference.edit().clear().apply()
 
-        showLogin()
-
-    }
-
-    private fun showLogin() {
-        findViewById<TextView>(R.id.tv1).text = "Login"
-        findViewById<EditText>(R.id.et1).isEnabled = true
-
-        val btn = findViewById<Button>(R.id.btn1)
-        btn.text = "Login"
-        btn.setOnClickListener {
-            login(it)
-        }
-        Log.d("Shared_pref_aula","found shared pref")
-    }
-
-    fun login(view: View) {
+    fun save(view: View) {
         val editTextUsername = findViewById<EditText>(R.id.et1)
 
         if(editTextUsername.text.isNotEmpty()){
             val sharedPreference: SharedPreferences = getSharedPreferences("FILE_1",Context.MODE_PRIVATE)
             sharedPreference.edit()
-                .putString("PREF_USERNAME",editTextUsername.text.toString())
+                .putString("userInput",editTextUsername.text.toString())
                 .apply()
 
-            showWelcome(editTextUsername.text.toString())
-            Log.d("Shared_pref_aula","found shared pref: ${editTextUsername.text}")
+
         }else{
-            Toast.makeText(this, "Insert username", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Write something", Toast.LENGTH_SHORT).show()
         }
     }
 }
